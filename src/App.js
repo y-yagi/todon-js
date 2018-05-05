58319ae9941faa4c452a223d2ffda2d02bcbc206
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-05-05 11:23:52 +0900

    add need libraries

--- src/App.js
+++ src/App.js
@@ -5,12 +5,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="top">
+        <header className="top-header">
+          <h1 className="top-title">Welcome to Todon</h1>
         </header>
-        <p className="App-intro">
+        <p className="top-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
